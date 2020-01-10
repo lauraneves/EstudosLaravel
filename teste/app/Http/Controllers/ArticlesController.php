89ff51207632bb1d9453b0a32a9fd0772c05bbf9
lauraneves <lauraneves784@gmail.com>
2020-01-10 18:51:14 -0300

    episode 21

--- teste/app/Http/Controllers/ArticlesController.php
+++ teste/app/Http/Controllers/ArticlesController.php
@@ -7,17 +7,42 @@ use App\Article;
 
 class ArticlesController extends Controller
 {
-    public function show ($id) 
+    public function show ($id)  //shows a single one 
     {
         $article = Article::find($id);
 
         return view('articles.show', ['article'=>$article]);
     }
 
-    public function index ()
+    public function index ()    //shows a list
     {
         $article = Article::latest()->get();
 
         return view('articles.index', ['articles'=> $article]);
+    }
+    
+    public function create ()   //shows a view to create a new one
+    {
+
+    } 
+
+    public function store ()    //persist the new one
+    {
+
+    }
+
+    public function edit ()     //shows a view to edit an existing one
+    {
+
+    }
+
+    public function update ()   //persist the edit
+    {
+
+    }
+
+    public function destroy ()  //delete 
+    {
+
     }
 }
