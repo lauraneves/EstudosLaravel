27da542b1d8d070aa914c4b3031902e1796faf02
lauraneves <lauraneves784@gmail.com>
2020-01-13 14:13:49 -0300

    episode 31

--- teste/app/Article.php
+++ teste/app/Article.php
@@ -13,11 +13,16 @@ class Article extends Model
         return route('articles.show', $this);
     }
     
-    public function user()
+    public function author()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'user_id');
     }
-    
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
     // public function getRouteKeyName()
     // {
     //     return 'slug';
