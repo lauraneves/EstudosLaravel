fad45b85bcaf9a0fabb5184b20008e20b133f00f
lauraneves <lauraneves784@gmail.com>
2020-01-13 13:06:59 -0300

    episode 29

--- teste/app/User.php
+++ teste/app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use SebastianBergmann\CodeCoverage\Report\Xml\Project;
 
 class User extends Authenticatable
 {
@@ -36,4 +37,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function articles()
+    {
+        return $this->hasMany(Article::class);  //select * from articles where user_id = 1
+    }
+    
+    public function projects()
+    {
+        return $this->hasMany(Project::class);
+    }
 }
